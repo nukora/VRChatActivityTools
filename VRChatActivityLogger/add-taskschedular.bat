15fcabcd8f1148ff6e9f0c77ec9b7aeab0421caa
Narazaka <info@narazaka.net>
2021-02-07 17:25:35 +0900

    fix WorkingDirectory

--- VRChatActivityLogger/add-taskschedular.bat
+++ VRChatActivityLogger/add-taskschedular.bat
@@ -4,7 +4,7 @@ if exist task_actual.xml del task_actual.xml
 setlocal enabledelayedexpansion
 for /f "delims=" %%a in (task.xml) do (
   set line=%%a
-  echo !line:__COMMAND__="%~dp0VRChatActivityLogger.exe"! >> task_actual.xml
+  echo !line:__DIR__=%~dp0! >> task_actual.xml
 )
 
 schtasks /Create /F /XML task_actual.xml /TN VRChatActivityLogger
